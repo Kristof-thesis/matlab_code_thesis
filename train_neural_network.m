cb5e45dd9376ad5990ef0476ae1202e909ec83f9
Kristof-thesis <az_email_cim@amit_hasznaltal.com>
2026-03-25 16:27:10 +0100

    magic formulával és globális koordinátával kiegészítve

--- train_neural_network.m
+++ train_neural_network.m
@@ -2,7 +2,7 @@
 % 1. Adatok betöltése a munkaterületről
 try
     
-    required_vars = {'all_sim_delta', 'all_sim_vx', 'all_sim_vy', 'all_sim_omega', 'all_sim_ay', 'all_tout'};
+    required_vars = {'all_sim_delta', 'all_sim_vx', 'all_sim_vy', 'all_sim_omega', 'all_sim_ay', 'all_tout','all_sim_ax','all_sim_yawacc'};
     for i = 1:length(required_vars)
         if ~exist(required_vars{i}, 'var')
             error(['HIBA: A(z) ''' required_vars{i} ''' valtozo nem talalhato. Futtassa le a generate_training_data.m-et!']);
@@ -17,11 +17,13 @@ try
         all_sim_delta'; 
         all_sim_vx';    
         all_sim_omega'; 
+        all_sim_ax';
         all_sim_ay'     
     ];
 
     % Kimeneti adatok (Y)
-    targets = all_sim_vy'; 
+    targets = [all_sim_vy';
+               all_sim_yawacc'];
 
     numSamples = size(inputs, 2);
     if numSamples == 0
@@ -36,7 +38,7 @@ catch ME
 end
 
 %% 2. Neurális háló létrehozása és konfigurálása
-hiddenLayerSize = [8 5]; 
+hiddenLayerSize = [15 10]; 
 
 
 net = fitnet(hiddenLayerSize, 'trainbr'); 
@@ -45,7 +47,7 @@ net = fitnet(hiddenLayerSize, 'trainbr');
 net.trainParam.showWindow = true;       
 net.trainParam.showCommandLine = true;  
 net.trainParam.epochs = 500;           
-net.trainParam.goal = 1e-5;           
+net.trainParam.goal = 1e-6;           
 
 % Bayesian Regularization esetén a divideParam-ot a hálózat maga kezeli belsőleg,
 
@@ -66,14 +68,56 @@ save('net_carmaker_trained.mat', 'net');
 
 %% Eredmények megjelenítése 
 outputs = net(inputs); 
+vy_pred = outputs(1, :);
+ya_pred = outputs(2, :);
 
-% Rajzoljuk ki csak az időbeli lefutást (ez gyorsabb)
-figure('Name', 'Laterális sebesség ellenőrzés');
-plot(all_tout, targets, 'b', 'LineWidth', 1.5, 'DisplayName', 'CarMaker (Valóság)');
+%% 4. Trajektória rekonstrukció (XY sík)
+% Predikciók kinyerése
+nn_out = net(inputs); 
+vy_nn = nn_out(1,:); 
+ya_nn = nn_out(2,:);
+Ts = mean(diff(all_tout));
+numS = length(all_tout);
+
+% Inicializálás
+X_nn = zeros(numS,1); Y_nn = zeros(numS,1); Psi_nn = zeros(numS,1); om_nn = zeros(numS,1);
+X_ref = zeros(numS,1); Y_ref = zeros(numS,1); Psi_ref = zeros(numS,1);
+om_nn(1) = all_sim_omega(1); % Kezdő szögsebesség
+
+for t = 1:numS-1
+    % NN alapú integrálás 
+    om_nn(t+1)  = om_nn(t) + ya_nn(t) * Ts;
+    Psi_nn(t+1) = Psi_nn(t) + om_nn(t) * Ts;
+    X_nn(t+1)   = X_nn(t) + (all_sim_vx(t)*cos(Psi_nn(t)) - vy_nn(t)*sin(Psi_nn(t))) * Ts;
+    Y_nn(t+1)   = Y_nn(t) + (all_sim_vx(t)*sin(Psi_nn(t)) + vy_nn(t)*cos(Psi_nn(t))) * Ts;
+    
+    % CarMaker referencia integrálás 
+    Psi_ref(t+1) = Psi_ref(t) + all_sim_omega(t) * Ts;
+    X_ref(t+1)   = X_ref(t) + (all_sim_vx(t)*cos(Psi_ref(t)) - all_sim_vy(t)*sin(Psi_ref(t))) * Ts;
+    Y_ref(t+1)   = Y_ref(t) + (all_sim_vx(t)*sin(Psi_ref(t)) + all_sim_vy(t)*cos(Psi_ref(t))) * Ts;
+end
+
+% Megjelenítés
+figure('Name', 'NN Trajektória Validáció');
+plot(X_ref, Y_ref, 'b', 'LineWidth', 2, 'DisplayName', 'CarMaker (Ref)'); hold on;
+plot(X_nn, Y_nn, 'r--', 'LineWidth', 1.5, 'DisplayName', 'NN Rekonstrukció');
+title('Jármű útvonala az XY síkon (Tanított NN alapján)');
+xlabel('X [m]'); ylabel('Y [m]'); axis equal; grid on; legend show;
+% Első subplot: Vy
+figure
+plot(all_tout, targets(1,:), 'b', 'LineWidth', 1.8, 'DisplayName', 'CarMaker (Ref)');
 hold on;
-plot(all_tout, outputs, 'r--', 'LineWidth', 1.2, 'DisplayName', 'Neurális Háló');
-xlabel('Idő [s]');
+plot(all_tout, vy_pred, 'r--', 'LineWidth', 1.2, 'DisplayName', 'NN Predikció');
 ylabel('Vy [m/s]');
-title('A tanítás eredménye');
-legend show;
-grid on;
+title('Laterális sebesség becslése');
+legend show; grid on;
+
+% Második subplot: YawAcc
+figure
+plot(all_tout, targets(2,:), 'b', 'LineWidth', 1.8, 'DisplayName', 'CarMaker (Ref)');
+hold on;
+plot(all_tout, ya_pred, 'g--', 'LineWidth', 1.2, 'DisplayName', 'NN Predikció');
+xlabel('Idő [s]');
+ylabel('YawAcc [rad/s^2]');
+title('Legördülési gyorsulás becslése');
+legend show; grid on;
--- train_pinn.m
+++ train_pinn.m
@@ -1,6 +1,6 @@
 %% PINN_DUAL_TARGET_FINAL.m
 % -------------------------------------------------------------------------
-% 1. Adatkinyerés (YawAcc hozzáadva a targetekhez)
+% 1. Adatkinyerés 
 % -------------------------------------------------------------------------
 f_col = @(x) x(:); 
 all_tout       = f_col(out_vx.Time);
@@ -15,10 +15,10 @@ all_sim_ax     = f_col(out_ax.Data);
 % -------------------------------------------------------------------------
 % 2. Bemenetek és Targetek összeállítása
 % -------------------------------------------------------------------------
-% Bemenetek: [delta; vx; omega; ay; ax] -> (yawacc kikerült)
+% Bemenetek: [delta; vx; omega; ay; ax] 
 inputs = [all_sim_delta, all_sim_vx, all_sim_omega, all_sim_ay, all_sim_ax]';
 
-% Targetek: [vy; yawacc] -> (Két kimenet!)
+% Targetek: [vy; yawacc] 
 targets = [all_sim_vy, all_sim_yawacc]';
 
 % Normalizálás
@@ -28,20 +28,20 @@ inputs_norm = (inputs - mu_in) ./ sigma_in;
 dlInputs = dlarray(inputs_norm, 'CB');
 dlTargets = dlarray(targets, 'CB');
 
-% Pacejka paraméterek (maradnak a cftool-os értékek)
+% Pacejka paraméterek 
 params.B = 25.046; params.C = 1.138; params.D = 8272.4;
 params.m = 2108.0; params.Iz = 3954.288; params.lf = 1.47; params.lr = 1.50;
 
 % -------------------------------------------------------------------------
-% 3. Hálózat és Tanítás (Kimeneti réteg mérete most már 2)
+% 3. Hálózat és Tanítás 
 % -------------------------------------------------------------------------
 net = dlnetwork([
     featureInputLayer(size(inputs, 1))
-    fullyConnectedLayer(40) % Picit növelve a kapacitás a két cél miatt
+    fullyConnectedLayer(40) 
     tanhLayer
     fullyConnectedLayer(40)
     tanhLayer
-    fullyConnectedLayer(2) % Kimenet: [vy; yawacc]
+    fullyConnectedLayer(2) 
 ]);
 
 numEpochs = 2000; learningRate = 0.002; lambda = 0.05;
@@ -59,7 +59,7 @@ end
 toc;
 
 % -------------------------------------------------------------------------
-% 4. Plotolás (Két külön ábra a két kimenetnek)
+% 4. Plotolás 
 % -------------------------------------------------------------------------
 predictions = extractdata(forward(net, dlInputs));
 vy_pred = predictions(1, :);
@@ -78,14 +78,14 @@ plot(all_tout, ya_pred, 'g--');
 title('YawAcc (Legördülési gyorsulás) becslése'); ylabel('rad/s^2'); xlabel('Idő [s]'); grid on; legend('Ref','PINN');
 
 % -------------------------------------------------------------------------
-% 5. ModelLoss (Két kimenet kezelése)
+% 5. ModelLoss 
 % -------------------------------------------------------------------------
 function [loss, gradients] = modelLoss(net, dlInputs, dlTargets, p, lambda, raw_in)
     preds = forward(net, dlInputs);
     vy_pred = preds(1, :);
     ya_pred = preds(2, :);
     
-    % Adat-alapú hiba (mindkét kimenetre)
+    % Adat-alapú hiba 
     lossData = l2loss(vy_pred, dlTargets(1, :)) + l2loss(ya_pred, dlTargets(2, :));
     
     % Fizikai számítások
@@ -97,11 +97,54 @@ function [loss, gradients] = modelLoss(net, dlInputs, dlTargets, p, lambda, raw_
     Fyf = p.D * sin(p.C * atan(p.B * af));
     Fyr = p.D * sin(p.C * atan(p.B * ar));
     
-    % Fizikai kényszer: a hálózat által becsült ya_pred-et is összevetjük a fizikai képlettel!
     lossAccel = l2loss((Fyf + Fyr) / p.m, ay_meas);
-    lossMomentum = l2loss((Fyf * p.lf - Fyr * p.lr) / p.Iz, ya_pred); % A becsült yawacc-ot kényszerítjük
+    lossMomentum = l2loss((Fyf * p.lf - Fyr * p.lr) / p.Iz, ya_pred); 
     
     lossPhys = 0.5 * lossAccel + 0.5 * lossMomentum;
     loss = lossData + lambda * lossPhys;
     gradients = dlgradient(loss, net.Learnables);
 end
+% -------------------------------------------------------------------------
+% 5. Trajektória Rekonstrukció 
+% -------------------------------------------------------------------------
+vy_pred_all = extractdata(forward(net, dlInputs)); 
+% Ha a hálózatodnak 2 kimenete van, akkor a 2. sor a YawAcc 
+ya_pred_all = vy_pred_all(2, :);
+vy_pred_val = vy_pred_all(1, :);
+
+Ts = mean(diff(all_tout));
+numS = length(all_tout);
+
+% Inicializálás 
+X_p = zeros(numS, 1); Y_p = zeros(numS, 1); Psi_p = zeros(numS, 1);
+X_ref = zeros(numS, 1); Y_ref = zeros(numS, 1); Psi_ref = zeros(numS, 1);
+
+for t = 1:numS-1
+    % PINN alapú integrálás 
+    Psi_p(t+1) = Psi_p(t) + all_sim_omega(t) * Ts; 
+    dX_p = (all_sim_vx(t)*cos(Psi_p(t)) - vy_pred_val(t)*sin(Psi_p(t))) * Ts;
+    dY_p = (all_sim_vx(t)*sin(Psi_p(t)) + vy_pred_val(t)*cos(Psi_p(t))) * Ts;
+    X_p(t+1) = X_p(t) + dX_p;
+    Y_p(t+1) = Y_p(t) + dY_p;
+    
+    % Referencia (CarMaker) az összehasonlításhoz
+    Psi_ref(t+1) = Psi_ref(t) + all_sim_omega(t) * Ts;
+    dX_r = (all_sim_vx(t)*cos(Psi_ref(t)) - all_sim_vy(t)*sin(Psi_ref(t))) * Ts;
+    dY_r = (all_sim_vx(t)*sin(Psi_ref(t)) + all_sim_vy(t)*cos(Psi_ref(t))) * Ts;
+    X_ref(t+1) = X_ref(t) + dX_r;
+    Y_ref(t+1) = Y_ref(t) + dY_r;
+end
+
+% -------------------------------------------------------------------------
+% 6. Megjelenítés külön ablakokban 
+% -------------------------------------------------------------------------
+
+% 3. ÁBRA: Globális útvonal 
+figure('Name', 'Globális Trajektória');
+plot(X_ref, Y_ref, 'b', 'LineWidth', 2, 'DisplayName', 'Valódi út (Ref)'); hold on;
+plot(X_p, Y_p, 'r--', 'LineWidth', 1.5, 'DisplayName', 'PINN rekonstruált út');
+title('Jármű globális útvonala a pályán'); 
+xlabel('Globális X [m]'); ylabel('Globális Y [m]'); 
+axis equal; grid on; legend show;
+
+save('my_pinn_model.mat', 'net', 'mu_in', 'sigma_in');
